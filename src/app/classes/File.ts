a6f3e7cf20d1264d9712bbee0d712031cb8fd907
notawiz4rd <xamasa98@gmail.com>
2019-10-24 12:54:31 +0200

    added file/folder classes

---
+++ src/app/classes/File.ts
@@ -0,0 +1,9 @@
+export class File {
+  id: number;
+  name: string;
+  author: string;
+  type: string;
+  thumbnailPath: string;
+  creationDate: number;
+  comments: Comment[];
+}
---
+++ src/app/classes/Folder.ts
@@ -0,0 +1,6 @@
+export class Folder {
+  id: number;
+  name: string;
+  files: File[];
+  folders: Folder[];
+}
