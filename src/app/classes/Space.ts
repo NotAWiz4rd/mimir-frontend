765cfb96078493fdda97f7137c14ec16c5f1ea32
NotAWiz4rd <xamasa98@gmail.com>
2019-11-14 20:04:55 +0100

    replaced User objects in space with their IDs

--- src/app/classes/Space.ts
+++ src/app/classes/Space.ts
@@ -1,11 +1,10 @@
 import {Folder} from './Folder';
-import {User} from './User';
 
 export class Space {
   id: number;
   name: string;
   root: Folder;
-  owner: User;
-  users: User[];
+  owner: number;
+  users: number[];
   thumnbailId: number;
 }
