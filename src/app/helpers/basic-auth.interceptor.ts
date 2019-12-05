cdf72c4008309e99370a0039829be162388ff347
NotAWiz4rd <xamasa98@gmail.com>
2019-12-05 20:10:54 +0100

    just tiny bits of refactoring (please use alt+strg+L before committing!)

--- src/app/helpers/basic-auth.interceptor.ts
+++ src/app/helpers/basic-auth.interceptor.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { UserService } from '../services/user.service';
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {UserService} from '../services/user.service';
 
 @Injectable()
 export class BasicAuthInterceptor implements HttpInterceptor {
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // add authorization header with basic auth credentials if available
