af3fd3a17cf857d12418322c9beded6b048b325b
hidotp <38942179+hidotp@users.noreply.github.com>
2019-12-06 16:02:54 +0100

    Update Landing Page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../services/user.service';
 import {NavigationService} from '../../services/navigation.service';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
@@ -12,16 +13,28 @@ export class LandingPageComponent implements OnInit {
   value1 = false;
   value2 = false;
   value3 = false;
-
+  scrollTo: string;
   constructor(private userService: UserService,
-              private navigationService: NavigationService) {
+              private navigationService: NavigationService,
+              private vps: ViewportScroller) {
   }
 
   ngOnInit() {
   }
 
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngAfterViewChecked() {
+    this.vps.scrollToAnchor(this.scrollTo);
+    this.scrollTo = undefined;
+  }
+
   goToLogin() {
     this.navigationService.navigateToView('login');
   }
+
+  scroll(id) {
+    this.scrollTo = id;
+  }
+
 }
 
