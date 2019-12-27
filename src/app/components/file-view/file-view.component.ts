8e3f0699927adf7912711077ec146f9c479c6a0a
Pii <piapurr@freenet.de>
2019-12-27 22:52:49 +0100

    added details window for pictures (and also for text editor)

--- src/app/components/file-view/file-view.component.ts
+++ src/app/components/file-view/file-view.component.ts
@@ -1,8 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {File} from '../../classes/File';
 import {FileDataService} from "../../services/file-data.service";
-import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
+import {SafeHtml} from "@angular/platform-browser";
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {StaticTextService} from "../../services/static-text.service";
+import {LanguageService} from "../../services/language.service";
 
 @Component({
   selector: 'app-file-view',
@@ -17,42 +19,43 @@ export class FileViewComponent implements OnInit {
   isPicture: boolean;
   text: SafeHtml = 'test';
 
-  fileUrl: string;
+  fileUrl: string = 'https://media.giphy.com/media/sSgvbe1m3n93G/source.gif';
 
-  constructor(private fileViewService: FileDataService,
-              private domSanitizer:DomSanitizer) {
+  constructor(private fileDataService: FileDataService,
+              public staticTextService: StaticTextService,
+              public languageService: LanguageService) {
   }
 
 
   ngOnInit() {
     this.contentFileType = this.getFileContentType();
-    if(this.contentFileType.includes('jpg') || this.contentFileType.includes('png')){
+    if (this.contentFileType.includes('jpg') || this.contentFileType.includes('png')) {
       this.isPicture = true;
       this.setFileUrl();
-    }else if(this.contentFileType.includes('txt')){
+    } else if (this.contentFileType.includes('txt')) {
       this.isPicture = false;
       this.setText();
     }
   }
 
   setFileUrl(): void {
-    this.fileViewService.fetchImg(this.file.id).subscribe(base64Image => {
+    this.fileDataService.fetchImg(this.file.id).subscribe(base64Image => {
       this.fileUrl = base64Image;
     });
   }
 
-  setText(): void{
-    this.fileViewService.getTextFile(this.file.id).subscribe(data => {
+  setText(): void {
+    this.fileDataService.getTextFile(this.file.id).subscribe(data => {
       this.text = data;
     });
   }
 
   getFileContentType(): String {
     const fileName = this.file.name.split('.');
-    return fileName[fileName.length-1];
+    return fileName[fileName.length - 1];
   }
 
-  editText(){
+  editText() {
 
   }
 
